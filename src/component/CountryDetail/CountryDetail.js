fe240013ab6a2dc5f06bd3883cc0a8a682d34735
AMMFahim <fahimiiuc8145@gmail.com>
2021-12-14 13:34:18 +0600

    first Commit

--- src/component/CountryDetail/CountryDetail.js
+++ src/component/CountryDetail/CountryDetail.js
@@ -1,23 +1,33 @@
 import React, { useEffect , useState} from 'react';
 import { useParams } from 'react-router-dom';
+import './CountryDetail.css'
 
 const CountryDetail = () => {
     const {id} = useParams();
-    const [country, setCountry] = useState({});
+    const [country, setCountry] = useState([]);
 
     useEffect(()=>{
         const countryUrl = `https://restcountries.com/v3.1/name/${id}`
         fetch(countryUrl)
         .then(res=>res.json())
-        .then(data=> setCountry(data))
+        .then(data=> setCountry(data[0]))
     },[])
-    console.log(country,null);
-    // const {area, population} = country
+    if(country){
+        console.log(country,null);
+        console.log(country.area,null);
+    }
+    
+
+ 
     return (
-        <div>
+        <div  className="singleCountryBox">
             <h3>country detail page:{id}</h3>
-             <h4>Area: {country.area}</h4>
-            {/* <p>{population}</p> */}
+             {/* <h4>Area: {JSON.stringify(country)}</h4> */}
+             {/* <img src={country.flags.png} alt="" /> */}
+             <p>Area:{country.area}</p>
+             <p>Population:{country.population}</p>
+             <h3>Capital:{country.capital}</h3>
+
         </div>
     );
 };
